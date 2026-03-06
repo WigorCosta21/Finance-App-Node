3d90b9fa8b616d1c0893dbdd077e51e537a53088
Wigor <wigor21@hotmail.com>
2026-03-05 21:58:41 -0300

    feat: add UpdateUserUseCase

---
+++ src/useCases/update-user.ts
@@ -0,0 +1,47 @@
+import bcrypt from 'bcrypt'
+
+import { EmailAlreadyInUseError } from '../errors/user.js'
+import { PostgresGetUserByEmailRepository } from '../repositories/postgres/get-user-by-email.js'
+import { PostgresUpdateUserRepository } from '../repositories/postgres/update-user.js'
+
+interface IUpdateUserParams {
+    first_name?: string
+    last_name?: string
+    email?: string
+    password?: string
+}
+
+export class UpdateUserUseCase {
+    async execute(userId: string, updateUserParams: IUpdateUserParams) {
+        if (updateUserParams.email) {
+            const postgresGetUserByEmailRepository =
+                new PostgresGetUserByEmailRepository()
+
+            const userWithProviderEmail =
+                await postgresGetUserByEmailRepository.execute(
+                    updateUserParams.email,
+                )
+
+            if (userWithProviderEmail) {
+                throw new EmailAlreadyInUseError(updateUserParams.email)
+            }
+        }
+
+        const user = { ...updateUserParams }
+
+        if (updateUserParams.password) {
+            const hashedPassword = await bcrypt.hash(
+                updateUserParams.password,
+                10,
+            )
+
+            user.password = hashedPassword
+        }
+
+        const postgresUpdateUserRepository = new PostgresUpdateUserRepository()
+
+        const updatedUser = postgresUpdateUserRepository.execute(userId, user)
+
+        return updatedUser
+    }
+}

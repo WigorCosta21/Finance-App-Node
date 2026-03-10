93bb0ea0c0d3a9696a9a4ac205302654a44b4f15
Wigor <wigor21@hotmail.com>
2026-03-09 23:40:06 -0300

    feat: add transaction helpers function

--- src/controllers/transaction/create-transaction.ts
+++ src/controllers/transaction/create-transaction.ts
@@ -1,8 +1,6 @@
 import type { Request } from 'express'
-import validator from 'validator'
-
 import type { CreateTransactionParams } from '../../types/transaction.js'
-import { badRequest, created, serverError } from '../helpers/http.js'
+import { created, serverError } from '../helpers/http.js'
 import type { CreateTransactionUseCase } from '../../useCases/transaction/create-transaction.js'
 import {
     checkIdIsValid,
@@ -10,6 +8,12 @@ import {
     requiredFieldIsMissing,
     validateRequiredFields,
 } from '../helpers/index.js'
+import {
+    checkIfAmountIsValid,
+    checkIfTypeIsValid,
+    invalidAmountResponse,
+    invalidTypeResponse,
+} from '../helpers/transaction.js'
 
 export class CreateTransactionController {
     constructor(private createTransactionUseCase: CreateTransactionUseCase) {}
@@ -40,29 +44,16 @@ export class CreateTransactionController {
                 return invalidIdResponse()
             }
 
-            const amoutIsValid = validator.isCurrency(
-                params.amount.toString(),
-                {
-                    digits_after_decimal: [2],
-                    allow_negatives: false,
-                    decimal_separator: '.',
-                },
-            )
+            const amoutIsValid = checkIfAmountIsValid(params.amount)
 
             if (!amoutIsValid) {
-                return badRequest({
-                    message: 'The amount must be a valid currency.',
-                })
+                return invalidAmountResponse()
             }
 
-            const typeIsValid = ['EARNING', 'EXPENSE', 'INVESTMENT'].includes(
-                params.type,
-            )
+            const typeIsValid = checkIfTypeIsValid(params.type)
 
             if (!typeIsValid) {
-                return badRequest({
-                    message: 'The type must be EARNING, EXPENSE, INVESTMENT',
-                })
+                return invalidTypeResponse()
             }
 
             const trasaction =

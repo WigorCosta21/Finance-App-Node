729adc09c058987d6e26fe10a254b907655df6df
Wigor <wigor21@hotmail.com>
2026-03-15 01:23:17 -0300

    feat: add zod to create user controller

--- src/controllers/user/create-user.ts
+++ src/controllers/user/create-user.ts
@@ -1,19 +1,12 @@
 import type { Request } from 'express'
+import { ZodError } from 'zod'
+
 import { CreateUserUseCase } from '../../useCases/index.js'
 import { EmailAlreadyInUseError } from '../../errors/user.js'
-import {
-    checkIfEmailIsValid,
-    checkIfPasswordIsValid,
-    emailIsAlreadyInUserResponse,
-    invalidPasswordResponse,
-    badRequest,
-    created,
-    serverError,
-    validateRequiredFields,
-    requiredFieldIsMissing,
-} from '../helpers/index.js'
+import { badRequest, created, serverError } from '../helpers/index.js'
 
 import type { CreateUserParams } from '../../types/user.js'
+import { createUserSchema } from '../../schemas/index.js'
 
 export class CreateUserController {
     constructor(private createUserUseCase: CreateUserUseCase) {}
@@ -22,36 +15,21 @@ export class CreateUserController {
         try {
             const params = httpRequest.body
 
-            const requiredFields: (keyof CreateUserParams)[] = [
-                'first_name',
-                'last_name',
-                'email',
-                'password',
-            ]
-
-            const { ok: requiredFieldWereProvided, missingField } =
-                validateRequiredFields(params, requiredFields)
-
-            if (!requiredFieldWereProvided && missingField) {
-                return requiredFieldIsMissing(missingField)
-            }
-
-            const passwordIsValid = checkIfPasswordIsValid(params.password)
-
-            if (!passwordIsValid) {
-                return invalidPasswordResponse()
-            }
-
-            const emailIsValid = checkIfEmailIsValid(params.email)
-
-            if (!emailIsValid) {
-                return emailIsAlreadyInUserResponse()
-            }
+            await createUserSchema.parseAsync(params)
 
             const createdUser = await this.createUserUseCase.execute(params)
 
             return created(createdUser)
         } catch (error) {
+            if (error instanceof ZodError) {
+                return badRequest({
+                    message: error.issues.map((issue) => ({
+                        field: issue.path.join('.'),
+                        message: issue.message,
+                    })),
+                })
+            }
+
             if (error instanceof EmailAlreadyInUseError) {
                 return badRequest({
                     message: error.message,

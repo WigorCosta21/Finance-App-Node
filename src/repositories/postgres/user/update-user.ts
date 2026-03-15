a25238f5482061fb601d8e3a1ef024436b83dd00
Wigor <wigor21@hotmail.com>
2026-03-15 18:11:32 -0300

    feat: ensure UpdateUserRepository use prisma

--- src/repositories/postgres/user/update-user.ts
+++ src/repositories/postgres/user/update-user.ts
@@ -1,32 +1,22 @@
-import { PostgresHelper } from '../../../db/postgres/helper.js'
+import { prisma } from '../../../../prisma/prisma.js'
 import type { UpdateUserParams } from '../../../types/user.js'
 import type { IUpdateUserRepository } from '../../interfaces/user/update-user.js'
 
 export class PostgresUpdateUserRepository implements IUpdateUserRepository {
     async execute(userId: string, updateParams: UpdateUserParams) {
-        const updateFields: string[] = []
-        const updateValues: unknown[] = []
-
-        Object.keys(updateParams).forEach((key) => {
-            const value = updateParams[key as keyof UpdateUserParams]
-
-            if (value !== undefined) {
-                updateFields.push(`${key} = $${updateValues.length + 1}`)
-                updateValues.push(value)
-            }
+        const user = await prisma.user.update({
+            where: {
+                id: userId,
+            },
+            data: updateParams,
+            select: {
+                id: true,
+                first_name: true,
+                last_name: true,
+                email: true,
+            },
         })
 
-        updateValues.push(userId)
-
-        const updateQuery = `
-            UPDATE users
-            SET ${updateFields.join(',')}
-            WHERE id = $${updateValues.length}
-            RETURNING id, first_name, last_name, email
-        `
-
-        const updateUser = await PostgresHelper.query(updateQuery, updateValues)
-
-        return updateUser[0] ?? null
+        return user ?? null
     }
 }

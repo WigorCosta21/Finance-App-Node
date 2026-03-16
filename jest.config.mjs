27f6298d50ff369d2253e0b705a35ec2c522177f
Wigor <wigor21@hotmail.com>
2026-03-15 23:26:56 -0300

    feat: add initial jest setup

---
+++ jest.config.mjs
@@ -0,0 +1,21 @@
+import { createDefaultEsmPreset } from 'ts-jest'
+
+const presetConfig = createDefaultEsmPreset({
+  tsconfig: 'tsconfig.json',
+})
+
+/** @type {import('jest').Config} */
+export default {
+  ...presetConfig,
+  bail: true,
+  clearMocks: true,
+  coverageProvider: 'v8',
+  testEnvironment: 'node',
+  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^@prisma/generated/client$': '<rootDir>/prisma/generated/client.ts',
+    '^@prisma/generated/enums$': '<rootDir>/prisma/generated/enums.ts',
+    '^@prisma/generated/(.*)$': '<rootDir>/prisma/generated/$1',
+  },
+}

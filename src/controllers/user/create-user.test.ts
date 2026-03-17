fd23483655d91d57c41c4a04489f16ee3833b61b
Wigor <wigor21@hotmail.com>
2026-03-16 21:16:47 -0300

    test: ensure 201 is returned when user is created

---
+++ src/controllers/user/create-user.test.ts
@@ -0,0 +1,38 @@
+import type { Request } from 'express'
+import type { CreateUserParams, PublicUser } from '../../types/user.js'
+import { CreateUserController } from './create-user.js'
+
+describe('Create User Controller', () => {
+    class CreateUserUseCaseStub {
+        async execute(user: CreateUserParams): Promise<PublicUser> {
+            return {
+                id: '123',
+                first_name: user.first_name,
+                last_name: user.last_name,
+                email: user.email,
+            }
+        }
+    }
+
+    it('should return 201 when create a user successfully', async () => {
+        const createUserUseCaseSub = new CreateUserUseCaseStub()
+
+        const createUserController = new CreateUserController(
+            createUserUseCaseSub,
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'Name Test',
+                last_name: 'Last Name Test',
+                email: 'test@email.com',
+                password: '12345678',
+            },
+        } as Request<unknown, unknown, CreateUserParams>
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(201)
+        expect(result.body).not.toBeNull()
+    })
+})

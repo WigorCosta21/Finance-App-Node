bcafe36b166f71f38b5b83f0675f8cb8be3ae56a
Wigor <wigor21@hotmail.com>
2026-03-16 21:33:04 -0300

    test: ensure 400 is returned if password is less than 6 characters

--- src/controllers/user/create-user.test.ts
+++ src/controllers/user/create-user.test.ts
@@ -132,4 +132,24 @@ describe('Create User Controller', () => {
 
         expect(result.statusCode).toBe(400)
     })
+    it('should return 400 if password is less than 6 characters', async () => {
+        const createUserUseCaseSub = new CreateUserUseCaseStub()
+
+        const createUserController = new CreateUserController(
+            createUserUseCaseSub,
+        )
+
+        const httpRequest = {
+            body: {
+                first_name: 'First Name Test',
+                last_name: 'Last Name Test',
+                email: 'test',
+                password: '12345',
+            },
+        } as Request<unknown, unknown, CreateUserParams>
+
+        const result = await createUserController.execute(httpRequest)
+
+        expect(result.statusCode).toBe(400)
+    })
 })

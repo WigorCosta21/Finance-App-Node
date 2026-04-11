03173150b8214bd12ef5266dd1d36d7e6895808b
Wigor <wigor21@hotmail.com>
2026-04-11 19:12:29 -0300

    test: ensure GetUserBalanceController returns 500 if getUserBalanceUseCase throws

--- src/controllers/user/get-user-balance.test.ts
+++ src/controllers/user/get-user-balance.test.ts
@@ -1,5 +1,6 @@
 import type { Request } from 'express'
 import { faker } from '@faker-js/faker'
+import { jest } from '@jest/globals'
 import { GetUserBalanceController } from './get-user-balance.js'
 import type { UserBalance, UserIdParams } from '../../types/user.js'
 import type { IGetUserBalanceUseCase } from '../../useCases/interfaces/user/get-user-balance.js'
@@ -26,7 +27,7 @@ describe('GetUserBalanceController', () => {
         }
     }
 
-    const makeHttpRequst = (userId?: string) => {
+    const makeHttpRequest = (userId?: string) => {
         return {
             params: {
                 userId: userId ?? faker.string.uuid(),
@@ -37,7 +38,7 @@ describe('GetUserBalanceController', () => {
     it('should return 200 when getting user balance', async () => {
         const { sut } = makeSut()
 
-        const httpResponse = await sut.execute(makeHttpRequst())
+        const httpResponse = await sut.execute(makeHttpRequest())
 
         expect(httpResponse.statusCode).toBe(200)
     })
@@ -45,8 +46,20 @@ describe('GetUserBalanceController', () => {
     it('shound return 400 when userId is invalid', async () => {
         const { sut } = makeSut()
 
-        const result = await sut.execute(makeHttpRequst('invalid_id'))
+        const result = await sut.execute(makeHttpRequest('invalid_id'))
 
         expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 500 if GetUserBalanceUseCase throws', async () => {
+        const { sut, getUserBalanceUseCase } = makeSut()
+
+        jest.spyOn(getUserBalanceUseCase, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const result = await sut.execute(makeHttpRequest())
+
+        expect(result.statusCode).toBe(500)
     })
 })

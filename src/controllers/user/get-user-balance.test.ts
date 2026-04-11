334a32a43fd3dd88e2ef06ce8d46d20488ae367d
Wigor <wigor21@hotmail.com>
2026-04-11 18:57:42 -0300

    test: ensure GetUserBalanceController returns 200 when getting user balance

---
+++ src/controllers/user/get-user-balance.test.ts
@@ -0,0 +1,44 @@
+import type { Request } from 'express'
+import { faker } from '@faker-js/faker'
+import { GetUserBalanceController } from './get-user-balance.js'
+import type { UserBalance, UserIdParams } from '../../types/user.js'
+import type { IGetUserBalanceUseCase } from '../../useCases/interfaces/user/get-user-balance.js'
+
+describe('GetUserBalanceController', () => {
+    class GetUserBalanceUseCaseStub implements IGetUserBalanceUseCase {
+        async execute(): Promise<UserBalance> {
+            return {
+                balance: faker.number.int(),
+                earnings: faker.number.int(),
+                expenses: faker.number.int(),
+                investments: faker.number.int(),
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const getUserBalanceUseCase = new GetUserBalanceUseCaseStub()
+        const sut = new GetUserBalanceController(getUserBalanceUseCase)
+
+        return {
+            sut,
+            getUserBalanceUseCase,
+        }
+    }
+
+    const makeHttpRequst = (userId?: string) => {
+        return {
+            params: {
+                userId: userId ?? faker.string.uuid(),
+            },
+        } as Request<UserIdParams>
+    }
+
+    it('should return 200 when getting user balance', async () => {
+        const { sut } = makeSut()
+
+        const httpResponse = await sut.execute(makeHttpRequst())
+
+        expect(httpResponse.statusCode).toBe(200)
+    })
+})

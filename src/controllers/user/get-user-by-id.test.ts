d1f084351bd35848535c6543a97493aad2bd419d
Wigor <wigor21@hotmail.com>
2026-04-12 19:10:26 -0300

    test: ensure GetUserByIdController returns 404 if a user is not found

--- src/controllers/user/get-user-by-id.test.ts
+++ src/controllers/user/get-user-by-id.test.ts
@@ -1,11 +1,12 @@
 import type { Request } from 'express'
 import { faker } from '@faker-js/faker'
+import { jest } from '@jest/globals'
 import { GetUserByIdController } from './get-user-by-id.js'
 import type { PublicUser, UserIdParams } from '../../types/user.js'
 
 describe('GetUserByIdController', () => {
     class GetUserByIdUseCaseSub {
-        async execute(): Promise<PublicUser> {
+        async execute(): Promise<PublicUser | null> {
             return {
                 id: faker.string.uuid(),
                 first_name: faker.person.firstName(),
@@ -16,11 +17,11 @@ describe('GetUserByIdController', () => {
     }
 
     const makeSut = () => {
-        const getUserByIdSub = new GetUserByIdUseCaseSub()
+        const getUserByIdUseCase = new GetUserByIdUseCaseSub()
 
-        const sub = new GetUserByIdController(getUserByIdSub)
+        const sub = new GetUserByIdController(getUserByIdUseCase)
 
-        return { getUserByIdSub, sub }
+        return { getUserByIdUseCase, sub }
     }
 
     const makeHttpRequest = (userId?: string) => {
@@ -39,11 +40,21 @@ describe('GetUserByIdController', () => {
         expect(result.statusCode).toBe(200)
     })
 
-    it('should return 400 if an invalid id is provider', async () => {
+    it('should return 400 if an invalid id is provided', async () => {
         const { sub } = makeSut()
 
         const result = await sub.execute(makeHttpRequest('invalid_id'))
 
         expect(result.statusCode).toBe(400)
+    })
+
+    it('shold return 404 if a user if not found', async () => {
+        const { sub, getUserByIdUseCase } = makeSut()
+
+        jest.spyOn(getUserByIdUseCase, 'execute').mockResolvedValue(null)
+
+        const result = await sub.execute(makeHttpRequest())
+
+        expect(result.statusCode).toBe(404)
     })
 })

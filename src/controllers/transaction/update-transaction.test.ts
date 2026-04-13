a35a6eba54724dff3f289275c945c280c892a2dc
Wigor <wigor21@hotmail.com>
2026-04-13 17:00:34 -0300

    test: ensure UpdateTransactionController returns 400 when unallowed field are provided

--- src/controllers/transaction/update-transaction.test.ts
+++ src/controllers/transaction/update-transaction.test.ts
@@ -63,4 +63,16 @@ describe('UpdateTransactionController', () => {
 
         expect(response.statusCode).toBe(400)
     })
+
+    it('should return 400 when unallowed field is provided', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute(
+            makeHttpRequestBody(faker.string.uuid(), {
+                unallowed_field: 'unallowed_field',
+            }),
+        )
+
+        expect(response.statusCode).toBe(400)
+    })
 })

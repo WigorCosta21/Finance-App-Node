ca42f41ee264c8b6fc4a7d2208ba5d46b7bf8499
Wigor <wigor21@hotmail.com>
2026-04-13 16:55:02 -0300

    test: ensure UpdateTransactionController returns 200 when updating a transaction successfully

---
+++ src/controllers/transaction/update-transaction.test.ts
@@ -0,0 +1,58 @@
+import { type Request } from 'express'
+import { faker } from '@faker-js/faker'
+import type {
+    Transaction,
+    TransactionIdParams,
+    UpdateTransactionParams,
+} from '../../types/transaction.js'
+import { UpdateTransactionController } from './update-transaction.js'
+
+describe('UpdateTransactionController', () => {
+    class UpdateTransactionUseCaseStub {
+        async execute(
+            _transactionId: string,
+            updateTransactionParams: UpdateTransactionParams,
+        ): Promise<Transaction | null> {
+            return {
+                id: faker.string.uuid(),
+                user_id: faker.string.uuid(),
+                name:
+                    updateTransactionParams.name ??
+                    faker.commerce.productName(),
+                date:
+                    updateTransactionParams.date ??
+                    faker.date.anytime.toString(),
+                amount:
+                    updateTransactionParams.amount ??
+                    Number(faker.finance.amount()),
+                type: updateTransactionParams.type ?? 'EARNING',
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const updateTransactionUseCase = new UpdateTransactionUseCaseStub()
+        const sut = new UpdateTransactionController(updateTransactionUseCase)
+
+        return { updateTransactionUseCase, sut }
+    }
+
+    const makeHttpRequestBody = (transactionId?: string, body?: unknown) => {
+        return {
+            params: {
+                transactionId: transactionId ?? faker.string.uuid(),
+            },
+            body: body ?? {
+                name: faker.commerce.productName(),
+            },
+        } as Request<TransactionIdParams, unknown, UpdateTransactionParams>
+    }
+
+    it('should return 200 when updating a transaction successfully', async () => {
+        const { sut } = makeSut()
+
+        const response = await sut.execute(makeHttpRequestBody())
+
+        expect(response.statusCode).toBe(200)
+    })
+})

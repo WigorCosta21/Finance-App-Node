c59c7a2a17ede3b90919f8573b0e346b9352eed3
Wigor <wigor21@hotmail.com>
2026-04-13 13:00:52 -0300

    test: ensure DeleteTransactionController returns 200 when deleting a transaction successfully

---
+++ src/controllers/transaction/delete-transaction.test.ts
@@ -0,0 +1,46 @@
+import type { Request } from 'express'
+import { faker } from '@faker-js/faker'
+import type {
+    Transaction,
+    TransactionIdParams,
+} from '../../types/transaction.js'
+import { DeleteTransactionController } from './delete-transaction.js'
+
+describe('DeleteTransactionController', () => {
+    class DeleteTransactionUseCaseStub {
+        async execute(): Promise<Transaction | null> {
+            return {
+                id: faker.string.uuid(),
+                user_id: faker.string.uuid(),
+                name: faker.commerce.productName(),
+                date: faker.date.anytime.toString(),
+                amount: Number(faker.finance.amount()),
+                type: 'EARNING',
+            }
+        }
+    }
+
+    const makeSut = () => {
+        const deleteTransactionUseCase = new DeleteTransactionUseCaseStub()
+
+        const sub = new DeleteTransactionController(deleteTransactionUseCase)
+
+        return { deleteTransactionUseCase, sub }
+    }
+
+    const makeHttpRequest = (transactionId?: string) => {
+        return {
+            params: {
+                transactionId: transactionId ?? faker.string.uuid(),
+            },
+        } as Request<TransactionIdParams>
+    }
+
+    it('should return 200 when deleting a transaction successfully', async () => {
+        const { sub } = makeSut()
+
+        const result = await sub.execute(makeHttpRequest())
+
+        expect(result.statusCode).toBe(200)
+    })
+})

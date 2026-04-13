40e31e2d2f3e46870215e2c8057a1204de1912a2
Wigor <wigor21@hotmail.com>
2026-04-13 08:15:45 -0300

    test: ensure UpdateUserController returns 400 if an unallowed field is provided

--- src/controllers/user/update-user.test.ts
+++ src/controllers/user/update-user.test.ts
@@ -31,7 +31,7 @@ describe('UpdateUserController', () => {
         return { updateUserUseCase, sut }
     }
 
-    const makeHttpRequestBody = (body?: UpdateUserParams, userId?: string) => {
+    const makeHttpRequestBody = (body?: unknown, userId?: string) => {
         return {
             params: {
                 userId: userId ?? faker.string.uuid(),
@@ -79,6 +79,28 @@ describe('UpdateUserController', () => {
 
         const result = await sut.execute(
             makeHttpRequestBody(undefined, 'invalid_id'),
+        )
+
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an invalid id is provided', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(
+            makeHttpRequestBody(undefined, 'invalid_id'),
+        )
+
+        expect(result.statusCode).toBe(400)
+    })
+
+    it('should return 400 when an unallowed_field is provided', async () => {
+        const { sut } = makeSut()
+
+        const result = await sut.execute(
+            makeHttpRequestBody({
+                unallowed_field: 'unallowed_field',
+            }) as Request<UserIdParams, unknown, UpdateUserParams>,
         )
 
         expect(result.statusCode).toBe(400)

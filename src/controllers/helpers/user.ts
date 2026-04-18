1fdc0afde79562bbc150fe82f637c3dbda1d5579
Wigor <wigor21@hotmail.com>
2026-04-18 13:08:12 -0300

    refactor: remove unused code

--- src/controllers/helpers/user.ts
+++ src/controllers/helpers/user.ts
@@ -1,16 +1,4 @@
-import { badRequest, notFound } from './http.js'
-
-export const invalidPasswordResponse = () => {
-    return badRequest({
-        message: 'Password must be at least 6 characters',
-    })
-}
-
-export const emailIsAlreadyInUserResponse = () => {
-    return badRequest({
-        message: 'Invalid e-mail. Please provide a valid one.',
-    })
-}
+import { notFound } from './http.js'
 
 export const userNotFoundRespose = () => {
     return notFound('User not found')
--- src/controllers/helpers/validation.ts
+++ src/controllers/helpers/validation.ts
@@ -16,29 +16,3 @@ export const requiredFieldIsMissing = (field: string) => {
         message: `The field ${field} is required.`,
     })
 }
-export const validateRequiredFields = <T extends object>(
-    params: T,
-    requiredFields: (keyof T)[],
-) => {
-    for (const field of requiredFields) {
-        const value = params[field]
-
-        const fieldIsMissing = value === undefined || value === null
-
-        const fieldIsEmpty =
-            typeof value === 'string' &&
-            validator.isEmpty(value, { ignore_whitespace: true })
-
-        if (fieldIsMissing || fieldIsEmpty) {
-            return {
-                missingField: field,
-                ok: false,
-            }
-        }
-    }
-
-    return {
-        missingField: undefined,
-        ok: true,
-    }
-}

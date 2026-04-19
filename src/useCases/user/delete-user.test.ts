82d80719fedd83b5dce9bfcc1d953a91f41042c4
Wigor <wigor21@hotmail.com>
2026-04-19 11:25:14 -0300

    test: ensure DeleteUserUseCase throws DeleteUserRepository throws

--- src/useCases/user/delete-user.test.ts
+++ src/useCases/user/delete-user.test.ts
@@ -42,4 +42,15 @@ describe('DeleteUserUseCase', () => {
 
         expect(executeSpy).toHaveBeenCalledWith(userId)
     })
+
+    it('should throw if DeleteUserRepository throws', async () => {
+        const { sut, deleteUserRepository } = makeSut()
+        jest.spyOn(deleteUserRepository, 'execute').mockRejectedValueOnce(
+            new Error(),
+        )
+
+        const promise = sut.execute(user.id)
+
+        await expect(promise).rejects.toThrow()
+    })
 })

1261818847ac5ecfc83ed7915a25acc4221edf61
Wigor <wigor21@hotmail.com>
2026-04-26 13:38:24 -0300

    test: ensure PostgresGetUserBalanceRepository add case for zero amount investment returning zero balance

--- src/repositories/postgres/user/get-user-balance.test.ts
+++ src/repositories/postgres/user/get-user-balance.test.ts
@@ -66,4 +66,27 @@ describe('PostgresGetUserBalanceRepository', () => {
         expect(result.investments.toString()).toBe('6000')
         expect(result.balance.toString()).toBe('2000')
     })
+
+    it('should return 0 when transaction type exists but amount is null', async () => {
+        const user = makeUserParams()
+        const createdUser = await prisma.user.create({ data: user })
+
+        await prisma.transaction.create({
+            data: {
+                name: faker.commerce.productName(),
+                amount: 0,
+                type: 'INVESTMENT',
+                date: faker.date.recent(),
+                user_id: createdUser.id,
+            },
+        })
+
+        const sut = new PostgresGetUserBalanceRepository()
+        const result = await sut.execute(createdUser.id)
+
+        expect(result.investments).toBe(0)
+        expect(result.earnings).toBe(0)
+        expect(result.expenses).toBe(0)
+        expect(result.balance).toBe(0)
+    })
 })

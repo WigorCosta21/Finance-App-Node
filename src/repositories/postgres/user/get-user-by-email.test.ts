8303554659aaaa425805fc23783a5c3c1d81c81a
Wigor <wigor21@hotmail.com>
2026-04-26 14:05:03 -0300

    test: ensure PostgresGetUserByEmailRepository gets user by email db

---
+++ src/repositories/postgres/user/get-user-by-email.test.ts
@@ -0,0 +1,20 @@
+import { prisma } from '../../../../prisma/prisma.js'
+import { makeUserParams } from '../../../tests/fixtures/user.js'
+import { PostgresGetUserByEmailRepository } from './get-user-by-email.js'
+
+describe('GetUserByEmail', () => {
+    let createdUser: Awaited<ReturnType<typeof prisma.user.create>>
+
+    beforeEach(async () => {
+        const user = makeUserParams()
+        createdUser = await prisma.user.create({ data: user })
+    })
+
+    it('should GetUserByEmaul on db', async () => {
+        const sut = new PostgresGetUserByEmailRepository()
+
+        const result = await sut.execute(createdUser.email)
+
+        expect(result).toStrictEqual(createdUser)
+    })
+})

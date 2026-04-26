6a61327a7f0948e880652aff703902c5114146f7
Wigor <wigor21@hotmail.com>
2026-04-26 14:35:32 -0300

    test: ensure PostgresUpdateUserRepository calls Prisma with correct params

--- src/repositories/postgres/user/update-user.test.ts
+++ src/repositories/postgres/user/update-user.test.ts
@@ -1,31 +1,53 @@
 import { faker } from '@faker-js/faker'
+import { jest } from '@jest/globals'
 import { prisma } from '../../../../prisma/prisma.js'
 import { makeUserParams } from '../../../tests/fixtures/user.js'
 import { PostgresUpdateUserRepository } from './update-user.js'
 import type { UpdateUserParams } from '../../../types/user.js'
 
 describe('GetUserByEmailRepository', () => {
-    let createdUser: Awaited<ReturnType<typeof prisma.user.create>>
+    let fakerUser: Awaited<ReturnType<typeof prisma.user.create>>
 
     beforeEach(async () => {
         const user = makeUserParams()
-        createdUser = await prisma.user.create({ data: user })
+        fakerUser = await prisma.user.create({ data: user })
     })
+
+    const updateUserParams: UpdateUserParams = {
+        first_name: faker.person.firstName(),
+        last_name: faker.person.lastName(),
+        email: faker.internet.email(),
+    }
 
     it('should GetUpdateUserRepository on db', async () => {
         const sut = new PostgresUpdateUserRepository()
 
-        const updateUserParams: UpdateUserParams = {
-            first_name: faker.person.firstName(),
-            last_name: faker.person.lastName(),
-            email: faker.internet.email(),
-        }
-
-        const result = await sut.execute(createdUser.id, updateUserParams)
+        const result = await sut.execute(fakerUser.id, updateUserParams)
 
         expect(result).toStrictEqual({
-            id: createdUser.id,
+            id: fakerUser.id,
             ...updateUserParams,
+        })
+    })
+
+    it('should call Prisma with correct params', async () => {
+        const sut = new PostgresUpdateUserRepository()
+
+        const prismaSpy = jest.spyOn(prisma.user, 'update')
+
+        await sut.execute(fakerUser.id, updateUserParams)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: fakerUser.id,
+            },
+            data: updateUserParams,
+            select: {
+                id: true,
+                first_name: true,
+                last_name: true,
+                email: true,
+            },
         })
     })
 })

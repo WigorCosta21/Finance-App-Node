c7b5de48d63644ef86927e87b552f99ccea9fb11
Wigor <wigor21@hotmail.com>
2026-05-03 20:00:27 -0300

    test: ensure PostgresUpdateTransactionRepository throws if prisma throws

--- src/repositories/postgres/transaction/update-transaction.test.ts
+++ src/repositories/postgres/transaction/update-transaction.test.ts
@@ -73,4 +73,15 @@ describe('PostgresUpdateTransactionRepository', () => {
             },
         })
     })
+
+    it('should throw if Prisma throws', async () => {
+        const sut = new PostgresUpdateTransactionRepository()
+        const prismaSpy = jest
+            .spyOn(prisma.transaction, 'update')
+            .mockRejectedValueOnce(new Error())
+
+        const promise = sut.execute(transaction.id, transaction)
+
+        await expect(promise).rejects.toThrow()
+    })
 })

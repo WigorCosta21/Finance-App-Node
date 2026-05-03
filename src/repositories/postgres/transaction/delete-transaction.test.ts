05af39aac26bcbeb559acab9552420995f37451f
Wigor <wigor21@hotmail.com>
2026-05-03 19:07:41 -0300

    test: ensure PostgresDeleteTransactionRepository calls Prisma with correct params

--- src/repositories/postgres/transaction/delete-transaction.test.ts
+++ src/repositories/postgres/transaction/delete-transaction.test.ts
@@ -1,17 +1,17 @@
+import { jest } from '@jest/globals'
 import { prisma } from '../../../../prisma/prisma.js'
 import { makeTransaction } from '../../../tests/fixtures/transactions.js'
 import { makeUserParams } from '../../../tests/fixtures/user.js'
 import { PostgresDeleteTransactionReposiroty } from './delete-transaction.js'
 
 describe('PostgresDeleteTransactionRepository', () => {
-    it('should delete a transaction on db', async () => {
-        const userFaker = makeUserParams()
+    const userFaker = makeUserParams()
+    const transactionFaker = makeTransaction()
 
+    it('should delete a transaction on db', async () => {
         const user = await prisma.user.create({
             data: userFaker,
         })
-
-        const transactionFaker = makeTransaction()
 
         const transaction = await prisma.transaction.create({
             data: {
@@ -30,5 +30,28 @@ describe('PostgresDeleteTransactionRepository', () => {
         expect(new Date(result!.date).toISOString().slice(0, 10)).toBe(
             new Date(transaction.date).toISOString().slice(0, 10),
         )
+    })
+
+    it('should call prisma with correnct params', async () => {
+        const user = await prisma.user.create({
+            data: userFaker,
+        })
+
+        const transaction = await prisma.transaction.create({
+            data: {
+                ...transactionFaker,
+                user_id: user.id,
+            },
+        })
+        const prismaSpy = jest.spyOn(prisma.transaction, 'delete')
+        const sut = new PostgresDeleteTransactionReposiroty()
+
+        await sut.execute(transaction.id)
+
+        expect(prismaSpy).toHaveBeenCalledWith({
+            where: {
+                id: transaction.id,
+            },
+        })
     })
 })

5cf355dd60772a3af093c784eb4590f3fc4865a2
Wigor <wigor21@hotmail.com>
2026-05-04 19:38:19 -0300

    test: ensure makeDeleteUserController returns valid DeleteUserController

--- src/factories/controllers/user.test.ts
+++ src/factories/controllers/user.test.ts
@@ -1,10 +1,12 @@
 import {
     CreateUserController,
+    DeleteUserController,
     GetUserByIdController,
     UpdateUserController,
 } from '../../controllers/index.js'
 import {
     makeCreateUserController,
+    makeDeleteUserController,
     makeGetUserByIdController,
     makeUpdateUserController,
 } from './user.js'
@@ -22,5 +24,9 @@ describe('UserControllerFactories', () => {
 
     it('should returna valid UpdateUserController instance', () => {
         expect(makeUpdateUserController()).toBeInstanceOf(UpdateUserController)
+    })
+
+    it('should returna valid DeleteUserController instance', () => {
+        expect(makeDeleteUserController()).toBeInstanceOf(DeleteUserController)
     })
 })

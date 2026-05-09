0d38200cd8884650df1f37c1f77731aee1deefe4
Wigor <wigor21@hotmail.com>
2026-05-09 14:24:55 -0300

    feat: ensure PostgresUpdateUserRepository throws UserNotFoundError if user not found

--- src/repositories/postgres/user/update-user.ts
+++ src/repositories/postgres/user/update-user.ts
@@ -1,22 +1,36 @@
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
 import { prisma } from '../../../../prisma/prisma.js'
 import type { UpdateUserParams } from '../../../types/user.js'
 import type { IUpdateUserRepository } from '../../interfaces/user/update-user.js'
+import { UserNotFoundError } from '../../../errors/user.js'
 
 export class PostgresUpdateUserRepository implements IUpdateUserRepository {
     async execute(userId: string, updateParams: UpdateUserParams) {
-        const user = await prisma.user.update({
-            where: {
-                id: userId,
-            },
-            data: updateParams,
-            select: {
-                id: true,
-                first_name: true,
-                last_name: true,
-                email: true,
-            },
-        })
+        try {
+            const user = await prisma.user.update({
+                where: {
+                    id: userId,
+                },
+                data: updateParams,
+                select: {
+                    id: true,
+                    first_name: true,
+                    last_name: true,
+                    email: true,
+                },
+            })
 
-        return user ?? null
+            return user ?? null
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                const code = error.code
+
+                if (code === 'P2025') {
+                    throw new UserNotFoundError(userId)
+                }
+            }
+
+            throw new Error()
+        }
     }
 }

26a09236f59c046ed2b5a84cfc4472eec04bb5ce
Wigor <wigor21@hotmail.com>
2026-05-09 14:38:23 -0300

    test: ensure POST /api/users should return 400 when email is already in use

--- src/routes/user.e2e.test.ts
+++ src/routes/user.e2e.test.ts
@@ -137,7 +137,7 @@ describe('User Routes E2E Testes', () => {
         expect(response.statusCode).toBe(404)
     })
 
-    it('PUTCH /api/users/:userId should return 404 when user is not found', async () => {
+    it('PATCH /api/users/:userId should return 404 when user is not found', async () => {
         const response = await request(app)
             .patch(`/api/users/${faker.string.uuid()}`)
             .send({
@@ -148,5 +148,25 @@ describe('User Routes E2E Testes', () => {
             })
 
         expect(response.statusCode).toBe(404)
+    })
+
+    it('POST /api/users should return 400 when the provided e-mail is already in use', async () => {
+        const user = makeUserParams()
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const response = await request(app)
+            .post(`/api/users`)
+            .send({
+                ...user,
+                id: undefined,
+                email: createdUser.email,
+            })
+
+        expect(response.statusCode).toBe(400)
     })
 })

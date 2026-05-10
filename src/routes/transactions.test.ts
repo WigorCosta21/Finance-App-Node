1363bcc243daae20dc78cb9e5cc7f0088e6f7c3b
Wigor <wigor21@hotmail.com>
2026-05-10 00:27:38 -0300

    test: PATCH /api/transactions/:transactionId returns 200 when updating a transaction successfully

--- src/routes/transactions.test.ts
+++ src/routes/transactions.test.ts
@@ -54,4 +54,31 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.status).toBe(200)
         expect(response.body[0].id).toBe(createdTransaction.id)
     })
+
+    it('PATCH /api/transactions/:transactionId should returns 200 when updating a transaction successfully', async () => {
+        const user = makeUserParams()
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+        const transaction = makeTransaction()
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({
+                ...transaction,
+                user_id: createdUser.id,
+                id: transaction.id,
+            })
+
+        const response = await request(app)
+            .patch(`/api/transactions/${createdTransaction.id}`)
+            .send({ amount: 100, type: TransactionType.INVESTMENT })
+
+        expect(response.status).toBe(200)
+        expect(response.body.amount).toBe(100)
+        expect(response.body.type).toBe('INVESTMENT')
+    })
 })

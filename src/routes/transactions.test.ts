a19b65409782cbbadc0bcd723aa269631c26ebe2
Wigor <wigor21@hotmail.com>
2026-05-10 00:27:09 -0300

    test: GET /api/transactions  returns when fetching transactions successfully

--- src/routes/transactions.test.ts
+++ src/routes/transactions.test.ts
@@ -2,9 +2,10 @@ import request from 'supertest'
 import { app } from '../app.js'
 import { makeTransaction } from '../tests/fixtures/transactions.js'
 import { makeUserParams } from '../tests/fixtures/user.js'
+import { TransactionType } from '../../generated/prisma/enums.js'
 
 describe('Transaction Routes E2E Tests', () => {
-    it('POST /api/transaction should return 201 when creating a transaction successfully', async () => {
+    it('POST /api/transactions should return 201 when creating a transaction successfully', async () => {
         const user = makeUserParams()
         const { body: createdUser } = await request(app)
             .post('/api/users')
@@ -26,5 +27,31 @@ describe('Transaction Routes E2E Tests', () => {
         expect(response.body.user_id).toBe(createdUser.id)
         expect(response.body.type).toBe(transaction.type)
         expect(response.body.amount).toBe(transaction.amount)
+    })
+
+    it('GET /api/transactions should returns 200 when fetching transactions successfully', async () => {
+        const user = makeUserParams()
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+        const transaction = makeTransaction()
+
+        const { body: createdTransaction } = await request(app)
+            .post('/api/transactions')
+            .send({
+                ...transaction,
+                user_id: createdUser.id,
+                id: transaction.id,
+            })
+
+        const response = await request(app).get(
+            `/api/transactions?userId=${createdUser.id}`,
+        )
+
+        expect(response.status).toBe(200)
+        expect(response.body[0].id).toBe(createdTransaction.id)
     })
 })

6eaf2fe843449efa76fa5cd317f436a98f55563b
Wigor <wigor21@hotmail.com>
2026-05-10 00:55:37 -0300

    feat: ensure PostgresUpdateTransactionRepository  throws TransactionNotFoundError if transaction is not found

--- src/repositories/postgres/transaction/update-transaction.ts
+++ src/repositories/postgres/transaction/update-transaction.ts
@@ -1,26 +1,40 @@
 import { prisma } from '../../../../prisma/prisma.js'
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime/client'
 import type {
     Transaction,
     UpdateTransactionParams,
 } from '../../../types/transaction.js'
 import type { IUpdateTransactionRepository } from '../../interfaces/transaction/update-transactions.js'
+import { TransactionNotFoundError } from '../../../errors/transaction.js'
 
 export class PostgresUpdateTransactionRepository implements IUpdateTransactionRepository {
     async execute(
         transactionId: string,
         updateTransactioParams: UpdateTransactionParams,
     ): Promise<Transaction | null> {
-        const updatedTransactions = await prisma.transaction.update({
-            where: {
-                id: transactionId,
-            },
-            data: updateTransactioParams,
-        })
+        try {
+            const updatedTransactions = await prisma.transaction.update({
+                where: {
+                    id: transactionId,
+                },
+                data: updateTransactioParams,
+            })
 
-        return {
-            ...updatedTransactions,
-            amount: updatedTransactions.amount.toNumber(),
-            date: updatedTransactions.date.toISOString(),
+            return {
+                ...updatedTransactions,
+                amount: updatedTransactions.amount.toNumber(),
+                date: updatedTransactions.date.toISOString(),
+            }
+        } catch (error) {
+            if (error instanceof PrismaClientKnownRequestError) {
+                const code = error.code
+
+                if (code === 'P2025') {
+                    throw new TransactionNotFoundError(transactionId)
+                }
+            }
+
+            throw new Error()
         }
     }
 }

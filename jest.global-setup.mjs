cbde07f52a080e20f0c1e6494e8b5e463cae6986
Wigor <wigor21@hotmail.com>
2026-05-16 16:46:56 -0300

    fix: skip docker-compose in jest setup when running in CI

--- jest.global-setup.mjs
+++ jest.global-setup.mjs
@@ -1,8 +1,13 @@
 import { execSync } from 'child_process'
 
 async function init() {
-    execSync('docker-compose up -d --wait postgres-test')
-    execSync('npx prisma db push')
+    if (!process.env.CI) {
+        execSync('docker compose up -d --wait postgres-test', {
+            stdio: 'inherit',
+        })
+    }
+
+    execSync('npx prisma db push', { stdio: 'inherit' })
 }
 
 export default init

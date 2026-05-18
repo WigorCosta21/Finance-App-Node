f224755095184cbe3dfee3a59ee91fba7823c36a
Wigor <wigor21@hotmail.com>
2026-05-17 23:25:44 -0300

    test: add login user routes

--- src/routes/user.e2e.test.ts
+++ src/routes/user.e2e.test.ts
@@ -175,4 +175,23 @@ describe('User Routes E2E Testes', () => {
 
         expect(response.statusCode).toBe(400)
     })
+
+    it('POST /api/users/login should return 200 and token when user credentials are valid', async () => {
+        const user = makeUserParams()
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({
+                ...user,
+                id: undefined,
+            })
+
+        const response = await request(app).post('/api/users/login').send({
+            email: createdUser.email,
+            password: user.password,
+        })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.tokens.accessToken).toBeDefined()
+        expect(response.body.tokens.refreshToken).toBeDefined()
+    })
 })

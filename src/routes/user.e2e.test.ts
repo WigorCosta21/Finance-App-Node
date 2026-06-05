8e322f1194da3ede4eae2fa6fc0dc90de5d91f12
Wigor <wigor21@hotmail.com>
2026-06-04 22:14:18 -0300

    test: add refresh token route

--- src/routes/user.e2e.test.ts
+++ src/routes/user.e2e.test.ts
@@ -146,4 +146,21 @@ describe('User Routes E2E Tests', () => {
         expect(response.body.tokens.accessToken).toBeDefined()
         expect(response.body.tokens.refreshToken).toBeDefined()
     })
+
+    it('POST /api/users/refresh-token should return 200 and new tokens when refresh token is valid', async () => {
+        const user = makeUserParams()
+        const { body: createdUser } = await request(app)
+            .post('/api/users')
+            .send({ ...user, id: undefined })
+
+        const response = await request(app)
+            .post('/api/users/refresh-token')
+            .send({
+                refreshToken: createdUser.tokens.refreshToken,
+            })
+
+        expect(response.statusCode).toBe(200)
+        expect(response.body.accessToken).toBeDefined()
+        expect(response.body.refreshToken).toBeDefined()
+    })
 })
